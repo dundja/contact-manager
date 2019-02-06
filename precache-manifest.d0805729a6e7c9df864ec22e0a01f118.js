3b3325b4295b9c8b50f3cdec674150e438855eef
Radivoje Dundjerovic <radivoje.dundjerovic@plus4u.net>
2019-02-06 16:50:39 +0100

    Updates

---
+++ precache-manifest.d0805729a6e7c9df864ec22e0a01f118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "59016dd716fe3a8aa83c",
+    "url": "/contact-manager/static/js/main.59016dd7.chunk.js"
+  },
+  {
+    "revision": "e16b2db0103e47e1f566",
+    "url": "/contact-manager/static/js/1.e16b2db0.chunk.js"
+  },
+  {
+    "revision": "59016dd716fe3a8aa83c",
+    "url": "/contact-manager/static/css/main.eabcb9ea.chunk.css"
+  },
+  {
+    "revision": "e16b2db0103e47e1f566",
+    "url": "/contact-manager/static/css/1.11ff45d4.chunk.css"
+  },
+  {
+    "revision": "45c94e7bc6c190017e6ce089b5192163",
+    "url": "/contact-manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dddab0c9390df33857e3a122e1019a29.js"
+  "/contact-manager/precache-manifest.d0805729a6e7c9df864ec22e0a01f118.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/contact-manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
